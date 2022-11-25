3ed17d21f35013fb7c54ba698844079a0c4cdbff
FeatherMcGraw <114053081+FeatherMcGraw@users.noreply.github.com>
2022-11-25 17:36:24 +0900

    motify3

--- app/src/main/java/com/example/a2paths/MainActivity.kt
+++ app/src/main/java/com/example/a2paths/MainActivity.kt
@@ -24,9 +24,40 @@ class MainActivity : AppCompatActivity() {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.main_frame, HomeFragment()).commit()
 
+        binding.bottomNavi.setOnItemReselectedListener { item ->
+            when (item.itemId) {
+                R.id.item_fragment1 -> {
+                    val transaction = supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.main_frame, HomeFragment()).commit()
+                }
+                R.id.item_fragment2 -> {
+                    val transaction = supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.main_frame, ChatFragment()).commit()
+                }
+                R.id.item_fragment3 -> { //수정필요
+                    val transaction = supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.main_frame, ConsultFragment()).commit()
+                }
+                R.id.item_fragment4 -> {
+                    val transaction = supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.main_frame, SettingFragment()).commit()
+                }
+            }
+        }
+    }
 
-        deleteAuthLogin()
+    override fun onBackPressed() {
+        if(System.currentTimeMillis() - mBackWait >=2000 ) {
+            mBackWait = System.currentTimeMillis()
+            Toast.makeText(this, "뒤로가기 버튼을 한번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
+        } else {
+            finish()
+        }
+    }
 
-        autoLogin()
+    override fun onDestroy() {
+        mBinding = null
+        super.onDestroy()
+    }
 
 }
